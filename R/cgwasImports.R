960d693df81a54439f97e8e668855c529db44264
TianTTL <xjgao.tttl@gmail.com>
2022-04-21 22:46:31 +0800

    v0.9.3

--- R/cgwasImports.R
+++ R/cgwasImports.R
@@ -1,5 +1,5 @@
 #' @import utils grDevices graphics stats
-#' @importFrom data.table fread fwrite
+#' @importFrom data.table data.table fread fwrite
 #' @importFrom foreach foreach %dopar%
 #' @importFrom doParallel registerDoParallel
 #' @importFrom parallel makeCluster stopCluster clusterEvalQ
